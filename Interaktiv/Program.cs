026d7eb8b9cb08c3ab944d92ba3c3dfef7d51411
Abdalhadi <abdalhadi.darkoushe@elev.ga.ntig.se>
2024-09-17 12:16:19 +0200

    jhbkjh

--- Interaktiv/Program.cs
+++ Interaktiv/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("välj en rikte där du vill gå till höger,vänster eller rakt fram?");
+string direction = Console.ReadLine();
+
+if (direction.ToLower() == "vänster")
+{
+   Console.WriteLine("här så hittar du allt möjligt");
+   Console.WriteLine("välj en vara");
+   string produkt = Console.ReadLine();
+   if (produkt == "bil")
+   {
+
+   }
+   
+}
+else if (direction.ToLower() == "höger")
+{
+   Console.WriteLine("här så kan du hitta alla vardadgs varor vad än du vill ha här så får du välja ändast 3 varor för vardags rumet");
+   Console.WriteLine("välj en av de varorna. soffa,tv,fötölje eller bord?");
+   string produkt = Console.ReadLine();
+    if (produkt == "soffa")
+    {
+
+    }
+}
+else if (direction.ToLower() == "rakt fram")
+{
+    Console.WriteLine("här så hittar du alla badrums varor vad än du vill ha till badrumet till ditt hus");
+    Console.WriteLine("välj en av de varorna. badkar,skåp och toalet?");
+   string produkt = Console.ReadLine();
+    if (produkt == "skåp")
+    {
+
+    }
+}
+Console.ReadLine();
--- Interaktiv/obj/Debug/net8.0/Interaktiv.AssemblyInfo.cs
+++ Interaktiv/obj/Debug/net8.0/Interaktiv.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Interaktiv")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cbb43d4bbe39c8a25427d5219418f1f51f5c822")]
 [assembly: System.Reflection.AssemblyProductAttribute("Interaktiv")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Interaktiv")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
